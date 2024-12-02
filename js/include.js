8d34e2b07d0b67f41dd0058fc17a07705d0fc05a
GameStuff <66188005+game-stuff-official@users.noreply.github.com>
2024-12-02 14:40:42 -0600

    Syling, catching js errors if jQuery is not loaded and indented index.html's content

--- js/include.js
+++ js/include.js
@@ -1,2 +1,6 @@
-$("#top-nav-holder").load("/gs/include/nav-bar.html")
-$("#footer-holder").load("/gs/include/footer.html")
+if(typeof $ == "function"){
+    $("#top-nav-holder").load("/gs/include/nav-bar.html")
+    $("#footer-holder").load("/gs/include/footer.html")
+}else{
+    throw new Error("It appears you forgot to include jQuery.");
+}
