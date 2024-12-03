74fa25812f19150a5a83811f4105fcc2e15e4f60
GameStuff <66188005+game-stuff-official@users.noreply.github.com>
2024-12-02 19:39:19 -0600

    push a new outline

--- js/include.js
+++ js/include.js
@@ -1,6 +1,7 @@
 if(typeof $ == "function"){
     $("#top-nav-holder").load("/gs/include/nav-bar.html")
     $("#footer-holder").load("/gs/include/footer.html")
+    $("#outline-holder").load("/gs/include/hjson-outline.html")
 }else{
     throw new Error("It appears you forgot to include jQuery.");
 }
